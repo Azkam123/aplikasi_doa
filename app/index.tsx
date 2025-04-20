00711e7d22601f428f9abffca229c77559508cd9
moh hasan <mohhasan9998@gmail.com>
2025-04-20 13:05:01 +0700

    test

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+// app/index.tsx
+import { useLayoutEffect } from "react";
+import { useNavigation } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>kholil</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: '#ff0000',
+  }
+})
